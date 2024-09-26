96c5df801c73c44b92af1364bd5a1ebf4a636905
vankh <vankh@DESKTOP-UP11P67>
2024-09-26 19:28:11 +0700

    first commit

---
+++ src/controller/Client.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import view.ChangePasswordFrm;
+import view.DashboardFrm;
+import view.DeleteAccountFrm;
+import view.FloorSearchFrm;
+import view.LoginFrm;
+import view.RegisterFrm;
+import view.RoomSearchFrm;
+
+/**
+ *
+ * @author admin
+ */
+public class Client {
+
+    public static LoginFrm loginFrm;
+    public static SocketHandle socketHandle;
+    public static DatagramSocket socket;
+    public static RegisterFrm registerFrm;
+    public static DashboardFrm dashboardFrm;
+    public static DeleteAccountFrm deleteAccountFrm;
+    public static ChangePasswordFrm changePasswordFrm;
+    public static RoomSearchFrm roomSearchFrm;
+    public static FloorSearchFrm floorSearchFrm;
+
+    public Client() {
+        try {
+            socket = new DatagramSocket();
+            loginFrm = new LoginFrm();
+            loginFrm.setVisible(true);
+            socketHandle = new SocketHandle(socket);
+            socketHandle.run();
+        } catch (SocketException | UnknownHostException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void OpenView(View viewName) {
+        switch (viewName) {
+            case LOGIN:
+                loginFrm = new LoginFrm();
+                loginFrm.setVisible(true);
+                break;
+            case REGISTER:
+                registerFrm = new RegisterFrm();
+                registerFrm.setVisible(true);
+                break;
+            case FLOOR_SEARCH:
+                floorSearchFrm = new FloorSearchFrm();
+                floorSearchFrm.setVisible(true);
+                break;
+            case ROOM_SEARCH:
+                roomSearchFrm = new RoomSearchFrm();
+                roomSearchFrm.setVisible(true);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+    public static void OpenView(View viewName, int x, int y) {
+        switch (viewName) {
+            case LOGIN:
+                loginFrm = new LoginFrm();
+                loginFrm.setLocation(x, y);
+                loginFrm.setVisible(true);
+                break;
+            case REGISTER:
+                registerFrm = new RegisterFrm();
+                registerFrm.setLocation(x, y);
+                registerFrm.setVisible(true);
+                break;
+            case FLOOR_SEARCH:
+                floorSearchFrm = new FloorSearchFrm();
+                floorSearchFrm.setLocation(x, y);
+                floorSearchFrm.setVisible(true);
+                break;
+            case ROOM_SEARCH:
+                roomSearchFrm = new RoomSearchFrm();
+                roomSearchFrm.setLocation(x, y);
+                roomSearchFrm.setVisible(true);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+    public static void OpenView(View viewName, String adminName) {
+        switch (viewName) {
+            case DASHBOARD:
+                dashboardFrm = new DashboardFrm(adminName);
+                dashboardFrm.setVisible(true);
+                break;
+            case DELETEACCOUNT:
+                deleteAccountFrm = new DeleteAccountFrm(adminName);
+                deleteAccountFrm.setVisible(true);
+                break;
+            case CHANGEPASSSWORD:
+                changePasswordFrm = new ChangePasswordFrm(adminName);
+                changePasswordFrm.setVisible(true);
+                break;
+            case FLOOR_SEARCH:
+                floorSearchFrm = new FloorSearchFrm(adminName);
+                floorSearchFrm.setVisible(true);
+                break;
+            case ROOM_SEARCH:
+                roomSearchFrm = new RoomSearchFrm(adminName);
+                roomSearchFrm.setVisible(true);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    public static void OpenView(View viewName, String adminName, int x, int y) {
+        switch (viewName) {
+            case DASHBOARD:
+                dashboardFrm = new DashboardFrm(adminName);
+                dashboardFrm.setLocation(x, y);
+                dashboardFrm.setVisible(true);
+                break;
+            case DELETEACCOUNT:
+                deleteAccountFrm = new DeleteAccountFrm(adminName);
+                deleteAccountFrm.setLocation(x, y);
+                deleteAccountFrm.setVisible(true);
+                break;
+            case CHANGEPASSSWORD:
+                changePasswordFrm = new ChangePasswordFrm(adminName);
+                changePasswordFrm.setLocation(x, y);
+                changePasswordFrm.setVisible(true);
+                break;
+            case FLOOR_SEARCH:
+                floorSearchFrm = new FloorSearchFrm(adminName);
+                floorSearchFrm.setLocation(x, y);
+                floorSearchFrm.setVisible(true);
+                break;
+            case ROOM_SEARCH:
+                roomSearchFrm = new RoomSearchFrm(adminName);
+                roomSearchFrm.setLocation(x, y);
+                roomSearchFrm.setVisible(true);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+    public static void CloseView(View viewName) {
+        switch (viewName) {
+            case LOGIN:
+                loginFrm.dispose();
+                break;
+            case DASHBOARD:
+                dashboardFrm.dispose();
+                break;
+            case REGISTER:
+                registerFrm.dispose();
+                break;
+            case DELETEACCOUNT:
+                deleteAccountFrm.dispose();
+                break;
+            case CHANGEPASSSWORD:
+                changePasswordFrm.dispose();
+                break;
+            case FLOOR_SEARCH:
+                floorSearchFrm.dispose();
+                break;
+            case ROOM_SEARCH:
+                roomSearchFrm.dispose();
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    public enum View {
+        LOGIN,
+        REGISTER,
+        DASHBOARD,
+        DELETEACCOUNT,
+        CHANGEPASSSWORD,
+        FLOOR_SEARCH,
+        ROOM_SEARCH
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
+}
---
+++ src/controller/SocketHandle.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author admin
+ */
+public class SocketHandle implements Runnable {
+
+    private final DatagramSocket socket;
+    private final InetAddress serverAddress;
+    
+    public SocketHandle(DatagramSocket socket) throws UnknownHostException {
+        this.socket = socket;
+        this.serverAddress = InetAddress.getByName("localhost");
+    }
+    
+    public void write(String message) {
+        try {
+            byte[] sendData = message.getBytes("UTF-8");
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9999);
+            socket.send(sendPacket);
+            System.out.println("Client: " + message);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+
+                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8");
+                String[] msg = receivedMessage.trim().split(" ");
+                String res = msg[0];
+                
+                // dang nhap thanh cong, tra ve thong tin admin
+                if (res.equals("login-success")) { 
+                    String adminName = msg[1];
+                    System.out.println(adminName);
+                    int x = Client.loginFrm.getLocation().x;
+                    int y = Client.loginFrm.getLocation().y;
+                    Client.CloseView(Client.View.LOGIN);
+                    Client.OpenView(Client.View.DASHBOARD, adminName, x, y);
+                }
+                // dang nhap that bai, yeu cau nhap lai
+                else if (res.equals("login-fail")) {
+                    Client.loginFrm.showErr("Nhập sai tên hoặc mật khẩu");
+                }
+                //dang ky thanh cong
+                else if (res.equals("register-success")) {
+                    int x = Client.registerFrm.getLocation().x;
+                    int y = Client.registerFrm.getLocation().y;
+                    Client.CloseView(Client.View.REGISTER);
+                    Client.OpenView(Client.View.LOGIN, x, y);
+                    Client.loginFrm.showErr("Bạn vừa đăng Ký thành công!!!");
+                }
+                // dang ky that bai
+                else if (res.equals("register-fail")) {
+                    Client.registerFrm.showErr("Tài khoản đã tồn tại, vui lòng nhập lại!");
+                }
+                
+                // xu ly xóa tài khoản
+                else if (res.equals("delete-account-success")) {
+                    int x = Client.deleteAccountFrm.getLocation().x;
+                    int y = Client.deleteAccountFrm.getLocation().y;
+                    Client.CloseView(Client.View.DELETEACCOUNT);
+                    Client.OpenView(Client.View.LOGIN,x,y);
+                    Client.loginFrm.showErr("Bạn vừa xóa tài khoản thành công!!!");
+                }
+                else if (res.equals("delete-account-fail")) {
+                    Client.deleteAccountFrm.showErr("Nhập sai mật khẩu.");
+                }
+                
+                // đổi password thành công
+                else if (res.equals("change-password-success")) {
+                    String name = msg[1];
+                    int x = Client.changePasswordFrm.getLocation().x;
+                    int y = Client.changePasswordFrm.getLocation().y;
+                    Client.CloseView(Client.View.CHANGEPASSSWORD);
+                    Client.OpenView(Client.View.DASHBOARD, name, x, y);
+                }
+                else if (res.equals("change-password-fail")) {
+                    Client.changePasswordFrm.showErr("Nhập sai mật khẩu!!");
+                }
+                
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
